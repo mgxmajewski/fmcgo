95a858f387b27a8f9385fd4383301f0b74ef5c05
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-03 18:03:32 +0200

    Create Layout component which provides global styles from using css module.

---
+++ src/components/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/normalize.css'
+import {layout} from "../styles/global.module.css";
+import NavBar from "./NavBar";
+// import '../styles/layout.css'
+import PropTypes from "prop-types";
+
+const Layout = ({children}) => {
+    return (
+        <div className={layout}>
+            <NavBar/>
+            {/*content for each page*/}
+            {children}
+            <footer>
+                <p>&reg; 2022 fmcgo.pl</p>
+            </footer>
+        </div>
+    );
+};
+
+Layout.propTypes = {
+    children: PropTypes.node
+};
+
+export default Layout;
