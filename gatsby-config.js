f25e959a2acb84c06c0526414c5b8e015765c1c0
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-03 21:17:48 +0200

    Install gatsby-plugin-react-svg plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,29 +2,37 @@ module.exports = {
     siteMetadata: {
         siteUrl: `https://www.yourdomain.tld`
     },
-    plugins: ["gatsby-plugin-image", {
-        resolve: 'gatsby-plugin-google-analytics',
-        options: {
-            "trackingId": "GTM-WB5FL6B"
-        }
-    }, "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-        resolve: 'gatsby-plugin-manifest',
-        options: {
-            "icon": "src/images/icon.png"
-        }
-    }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            "name": "images",
-            "path": "./src/images/"
+    plugins: [
+        "gatsby-plugin-image", {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                "trackingId": "GTM-WB5FL6B"
+            }
+        }, "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                "icon": "src/images/icon.png"
+            }
+        }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
+        }, {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "pages",
+                "path": "./src/pages/"
+            },
+            __key: "pages"
         },
-        __key: "images"
-    }, {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            "name": "pages",
-            "path": "./src/pages/"
-        },
-        __key: "pages"
-    }]
+        {
+            resolve: "gatsby-plugin-react-svg",
+            options: {
+                include: /assets/ // See below to configure properly
+            }
+        }
+    ]
 };
