d78dda66fe096eb5b4f8424e135892edc6a339ed
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-03 18:04:34 +0200

    Extract all index.js content to components.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,13 @@
 import * as React from "react"
-import "../../styles/index.css"
+import Layout from "../components/Layout";
+import DrawersContainer from "../components/DrawersContainer";
 
 // markup
 const IndexPage = () => {
     return (
-        <main>
-            <title>FMCgo</title>
-            <h1>
-                Hello FMCgo!
-            </h1>
-            <div className="drawers-container">
-                <div id="carrefour" className="container">
-                    <div className="cube">
-                        <div className="side front">Carrefour</div>
-                        <div className="side left"></div>
-                        <div className="side right"></div>
-                        <div className="side back"></div>
-                    </div>
-                </div>
-                <div id="pepsi" className="container">
-                    <div className="cube">
-                        <div className="side front">Pepsi</div>
-                        <div className="side left"></div>
-                        <div className="side right"></div>
-                        <div className="side back"></div>
-                    </div>
-                </div>
-                <div id="desperados" className="container">
-                    <div className="cube">
-                        <div className="side front">Desperados</div>
-                        <div className="side left"></div>
-                        <div className="side right"></div>
-                        <div className="side back"></div>
-                    </div>
-                </div>
-                <div id="heineken" className="container">
-                    <div className="cube">
-                        <div className="side front">Heineken</div>
-                        <div className="side left"></div>
-                        <div className="side right"></div>
-                        <div className="side back"></div>
-                    </div>
-                </div>
-                <div id="develey" className="container">
-                    <div className="cube">
-                        <div className="side front">Develey</div>
-                        <div className="side left"></div>
-                        <div className="side right"></div>
-                        <div className="side back"></div>
-                    </div>
-                </div>
-            </div>
-        </main>
+        <Layout>
+            <DrawersContainer/>
+        </Layout>
     )
 }
 
