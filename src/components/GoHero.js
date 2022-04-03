bc062470f865aee87871228a1beb0fe1326a3677
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-03 21:18:16 +0200

    Create GoHero component with styles.

--- src/components/GoHero.js
+++ src/components/GoHero.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Logo from '../assets/fmcgologo.svg'
 
-import {goHeroSection} from "../styles/go-hero.module.css";
+import {goHeroSection, fmcgoLogo, fmcgoTagline, fmcgoCopy} from "../styles/go-hero.module.css";
 
 const GoHero = () => {
     return (
         <section className={goHeroSection}>
-            Lorem Ipsum
+                <Logo className={fmcgoLogo}/>
+            <div className={fmcgoTagline}>
+                We <sup><u>move</u></sup> as fast <br/> as goods we <sup><u>move</u></sup>
+            </div>
+            <div className={fmcgoCopy}>We are FMC_Go – newly established advertising agency specialized in work for FMCG and retail brands.<br/>
+                We are a team of specialists and managers with many years of experience gathered in the biggest
+                international and local agencies.<br/> Our mission is to deliver high-end strategic and creative thinking
+                into everyday advertising for
+                everyday brands.
+            </div>
         </section>
     );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import Layout from "../components/Layout";
-import DrawersContainer from "../components/DrawersContainer";
+// import DrawersContainer from "../components/DrawersContainer";
 import GoHero from "../components/GoHero";
 
 // markup
