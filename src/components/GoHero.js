96988d58a596c86c6267288247d9faba997656ea
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-04 12:30:59 +0200

    Adjust GoHero layout and gradients.

--- src/components/GoHero.js
+++ src/components/GoHero.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import Logo from '../assets/fmcgologo.svg'
 
-import {goHeroSection, fmcgoLogo, fmcgoTagline, fmcgoCopy, fmcgoTaglineGradient} from "../styles/go-hero.module.css";
+import {
+    fmcgoCopy,
+    fmcgoLogo,
+    fmcgoTagline,
+    fmcgoTaglineGradientSecondary,
+    fmcgoTaglineGradientTricolor,
+    fmcgoVectorLogo,
+    goHeroSection
+} from "../styles/go-hero.module.css";
 
 const GoHero = () => {
     return (
         <section className={goHeroSection}>
-                <Logo className={fmcgoLogo}/>
-            <div className={fmcgoTagline}>
-                We <sup className={fmcgoTaglineGradient}>move</sup> as fast <br/> as goods we <sup className={fmcgoTaglineGradient}>move</sup>
+            <div className={fmcgoLogo}>
+                <Logo className={fmcgoVectorLogo}/>
             </div>
-            <div className={fmcgoCopy}>We are FMC_Go – newly established advertising agency specialized in work for FMCG and retail brands.<br/>
+            <div className={fmcgoTagline}>
+                We <sup className={fmcgoTaglineGradientTricolor}>move</sup> as fast <br/> as goods we <sup
+                className={fmcgoTaglineGradientSecondary}>move</sup>
+            </div>
+            <div className={fmcgoCopy}>We are FMC_Go – newly established advertising agency specialized in work for FMCG
+                and retail brands.<br/>
                 We are a team of specialists and managers with many years of experience gathered in the biggest
-                international and local agencies.<br/> Our mission is to deliver high-end strategic and creative thinking
+                international and local agencies.<br/> Our mission is to deliver high-end strategic and creative
+                thinking
                 into everyday advertising for
                 everyday brands.
             </div>
