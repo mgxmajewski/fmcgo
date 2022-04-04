e55389519bf1407576d226d3608e1ed3930f614c
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-04 23:11:02 +0200

    Fix cuboid walls arrangement (all walls facing outside). Wrong flips were causing minor misalignment for some sizes in marginal cases.

--- src/components/CoSlider.js
+++ src/components/CoSlider.js
@@ -9,7 +9,7 @@ const CoSlider = () => {
     return (
         <div className={coSlider}>
             <div className={sliderName}>Filip<sup>Szperl</sup></div>
-            <DrawerCuboid className={sliderLeftArrow} width='3.05vw'>&#60;</DrawerCuboid>
+            <DrawerCuboid className={sliderLeftArrow} width='3vw'>&#60;</DrawerCuboid>
             <div className={sliderCopy}><b>There’s 40 years of experience in advertising between us.</b> We’ve served as heads of departments, creative directors, senior creatives. For last 7 years we’ve been working together on hundreds of campaigns for brands from all categories imaginable. Along this way, we’ve discovered that what really turns us on is adverting that is meant to sell. </div>
             <DrawerCuboid className={sliderRightArrow} width='3vw'>&#62;</DrawerCuboid>
             <div className={sliderDots}>. .</div>
