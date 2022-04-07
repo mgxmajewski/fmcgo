7206a3db13c594891051d265c276342dad17b021
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-07 13:54:16 +0200

    Add contact details.

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -5,8 +5,13 @@ import {
     contactSection,
     contactTagline,
     formDetailsCol,
-    formInputCol
+    formInputCol,
+    detailsName,
+    detailsAddress,
+    detailsContact,
+    detailsSocial
 } from '../styles/contact-section.module.css'
+import {iconBox, socialIcons} from "../styles/radical.module.css";
 
 const ContactSection = () => {
     return (
@@ -17,8 +22,29 @@ const ContactSection = () => {
                     <div>Go</div>
                 </div>
                 <div className={contactForm}>
-                    <div className={formInputCol}>input</div>
-                    <div className={formDetailsCol}>details</div>
+                    <div className={formInputCol}>
+                        INPUT
+                    </div>
+                    <div className={formDetailsCol}>
+                        <div className={detailsName}>
+                            <b>FMC_GO</b><br/>
+                            30-333 Warszawa,<br/>
+                            Al Jerozolimskie 1
+                        </div>
+                        <div className={detailsAddress}>
+                            p.<br/>
+                            530 530 530<br/>
+                            +22 250 250 250
+                        </div>
+                        <div className={detailsContact}>
+                            m.<br/>
+                            office@fmcgo.pl
+                        </div>
+                        <div className={detailsSocial}>
+                            <div className={iconBox}>f</div>
+                            <div className={iconBox}>@</div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
