81ed1ee8fb0cf6778bacbee54580adb837ff65a9
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-12 12:56:50 +0200

    Fix typo in props destructuring.

--- src/components/Drawers/DrawerCuboidStatisGradient/DrawerCuboidStatisGradientElements.js
+++ src/components/Drawers/DrawerCuboidStatisGradient/DrawerCuboidStatisGradientElements.js
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const AdjustedDrawer = styled.div`
     --cube-width: ${({width}) => width};
-    --cube-color: ${(cubeColor)=> cubeColor};
-    --font-color: ${(fontColor) => fontColor};
+    --cube-color: ${({cubeColor})=> cubeColor};
+    --font-color: ${({fontColor}) => fontColor};
 `;
