9cfb27655601e3687d155b950691f3ddb43b591d
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-12 19:45:34 +0200

    Add window object availability for build purpose.

--- src/components/RadicalDrawersRow.js
+++ src/components/RadicalDrawersRow.js
@@ -9,7 +9,14 @@ import DrawerCuboidStatisGradient from "./Drawers/DrawerCuboidStatisGradient";
 
 const RadicalDrawersRow = () => {
 
-    const breakPoint = window.innerWidth
+    // Check if window is defined (so if in the browser or in node.js).
+    const isBrowser = typeof window !== "undefined"
+
+    let breakPoint;
+
+    if (isBrowser) {
+        breakPoint = window.innerWidth
+    }
 
     const drawerWidth = breakPoint > 900 ? '9vw' : '32vw'
     const drawerHeight = breakPoint > 900 ? '4.2vw' : '14vw'
@@ -17,36 +24,44 @@ const RadicalDrawersRow = () => {
 
     return (
         <>
-            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white' fontColor='black'>
+            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white'
+                                fontColor='black'>
                 <div className={beanContainer}>
                     <CoffeeBeanIcon className={bean}/>
                     <CoffeeBeanIcon className={bean}/>
                     <CoffeeBeanIcon className={bean}/>
                 </div>
             </DrawerCuboidStatic>
-            <DrawerCuboidStatisGradient width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='orange' fontColor='black'>
+            <DrawerCuboidStatisGradient width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='orange'
+                                        fontColor='black'>
                 <FmcgoTagIcon className={fmcgoTag}/>
             </DrawerCuboidStatisGradient>
-            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white' fontColor='black'>
+            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white'
+                                fontColor='black'>
                 <BottleIcon className={bottle}/>
             </DrawerCuboidStatic>
-            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white' fontColor='black'>
+            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white'
+                                fontColor='black'>
                 <span className={priceTag}>142<sup><u>99</u></sup></span>
             </DrawerCuboidStatic>
-            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white' fontColor='black'>
+            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white'
+                                fontColor='black'>
                 <div className={beanContainer}>
                     <CoffeeBeanIcon className={bean}/>
                     <CoffeeBeanIcon className={bean}/>
                     <CoffeeBeanIcon className={bean}/>
                 </div>
             </DrawerCuboidStatic>
-            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white' fontColor='black'>
+            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white'
+                                fontColor='black'>
                 <FmcgoTagIcon className={fmcgoTag}/>
             </DrawerCuboidStatic>
-            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white' fontColor='black'>
+            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white'
+                                fontColor='black'>
                 <span className={priceTag}>566<sup><u>60</u></sup></span>
             </DrawerCuboidStatic>
-            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white' fontColor='black'>
+            <DrawerCuboidStatic width={drawerWidth} height={drawerHeight} depth={drawerDepth} cubeColor='white'
+                                fontColor='black'>
                 <BottleIcon className={bottle}/>
             </DrawerCuboidStatic>
 
