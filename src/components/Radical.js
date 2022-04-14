b24138806dfbab55d4dbf9e0bd3815c26d87220a
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-14 12:06:41 +0200

    Make radical and co sections single column width consistent to global layout.

--- src/components/Radical.js
+++ src/components/Radical.js
@@ -18,7 +18,7 @@ const Radical = () => {
             </div>
             <div className={radicalTagline}>#Radical Advertising </div>
             <div className={radicalCopy}>
-                If a creative idea can’t be fit into an e-commerce banner, it’s not advertising.<br/>
+                If a creative idea can’t be fit into an e&#8209;commerce banner, it’s not advertising.<br/>
                 We are here to sell your products not just our services. We love our pack-shots and price-tags, and we’ll never hesitate <br/>
                 to use them. We believe that the only true measurement for successful advertising is sales.
             </div>
