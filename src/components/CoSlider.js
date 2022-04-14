3ce8cf49775e929e5b1ee45ab22bee28f63221b3
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-14 17:02:45 +0200

    Adjust slider layout for mobile.

--- src/components/CoSlider.js
+++ src/components/CoSlider.js
@@ -23,12 +23,9 @@ const CoSlider = () => {
                 </DrawerCuboidStatic>
             </div>
             <div className={sliderCopy}>
-                <b>There’s 40 years of experience in advertising between <br/>
-                    us.</b> We’ve served as heads of departments, creative directors, <br/>
-                senior creatives.<br/> For last 7 years we’ve been working together on hundreds of<br/>
-                campaigns for brands from all categories imaginable. Along this<br/>
-                way, we’ve discovered that what really turns us on is adverting<br/>
-                that is meant to sell.
+                <b>There’s 40 years of experience in advertising between us.</b> We’ve served as heads of departments, creative directors, senior creatives.<br/>
+                For last 7 years we’ve been working together on hundreds of campaigns for brands from all categories imaginable. Along this
+                way, we’ve discovered that what really turns us on is adverting that is meant to sell.
             </div>
             <div className={sliderRightArrow}>
                 <DrawerCuboidStatic
