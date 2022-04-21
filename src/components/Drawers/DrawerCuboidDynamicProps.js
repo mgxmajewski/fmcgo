c3f6f055ad75bcad1f24960fcc79a9cc8cbd4db8
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-21 02:24:40 +0200

    Revert changes to calc methods. Seems that the issue is not in calc but interpretation of "translate" properties.

--- src/components/Drawers/DrawerCuboidDynamicProps.js
+++ src/components/Drawers/DrawerCuboidDynamicProps.js
@@ -10,6 +10,35 @@
 
 import styled from "styled-components";
 
+const handleCustomWidthMobileNegative = brand => {
+    switch (brand) {
+        case "carrefour":
+            return "-32vw";
+        case "pepsi":
+            return "-23vw";
+        case "desperados":
+            return "-34vw";
+        case "heineken":
+            return "-30vw";
+        case "develey":
+            return "-28vw";
+        case "costa":
+            return "-36vw";
+        case "tchibo":
+            return "-26vw";
+        case "stock":
+            return "-23vw";
+        case "sephora":
+            return "-28vw";
+        case "clarins":
+            return "-27vw";
+        case "slider":
+            return "-9vw"
+        default:
+            return "-36vw";
+    }
+};
+
 const handleCustomWidthMobile = brand => {
     switch (brand) {
         case "carrefour":
@@ -36,6 +65,35 @@ const handleCustomWidthMobile = brand => {
             return "9vw"
         default:
             return "36vw";
+    }
+};
+
+const handleCustomWidthDesktopNegative = brand => {
+    switch (brand) {
+        case "carrefour":
+            return "-11.5vw";
+        case "pepsi":
+            return "-8.2vw";
+        case "desperados":
+            return "-11.2vw";
+        case "heineken":
+            return "-11.1vw";
+        case "develey":
+            return "-11vw";
+        case "costa":
+            return "-14vw";
+        case "tchibo":
+            return "-10vw";
+        case "stock":
+            return "-8.2vw";
+        case "sephora":
+            return "-11vw";
+        case "clarins":
+            return "-10.5vw";
+        case "slider":
+            return "-4.2vw"
+        default:
+            return "-9vw";
     }
 };
 
@@ -88,6 +146,7 @@ const handleCustomHeightMobile = section => {
 
 export const AdjustedDrawer = styled.div`
   --cube-width: ${({brand}) => handleCustomWidthMobile(brand)};
+  --negative-cube-width: ${({brand}) => handleCustomWidthMobileNegative(brand)};
   --cube-height: ${({section}) => handleCustomHeightMobile(section)};
   --cube-depth: ${({section}) => handleCustomDepthMobile(section)};
   --cube-color: ${({cubeColor}) => cubeColor};
@@ -95,6 +154,7 @@ export const AdjustedDrawer = styled.div`
 
   @media (min-width: 900px) {
     --cube-width: ${({brand}) => handleCustomWidthDesktop(brand)};
+    --negative-cube-width: ${({brand}) => handleCustomWidthDesktopNegative(brand)};
     --cube-height: 4.2vw;
     --cube-depth: 4vw;
   }
