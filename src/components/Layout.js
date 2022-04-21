d0d9e68b0820b5e50f35f8ebdd31b7a20c2ed28d
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-21 20:49:32 +0200

    Test react-spinner.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,18 +3,42 @@ import '../styles/normalize.css'
 import {layout} from "../styles/global.module.css";
 import NavBar from "./Navigation/NavBar";
 import PropTypes from "prop-types";
+import {css} from "@emotion/react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = css`
+  display: flex;
+  align-self: center;
+  justify-self: center;
+  margin: 0 auto;
+  border-color: #ffffff;
+`;
+
+// const loaderContainer = {
+//   display: "flex",
+//   align-self: "center",
+//   justify-self: "center",
+//   height: "100vh"
+// };
+
+const placeHolderStyle = {
+    background: "black",
+    display: "flex",
+    height: "100vh",
+    width: "100vw"
+};
 
 
-const NavLoadingPlaceHolder = () => {
-    const placeHolderStyle = {
-        height: "20vw",
-        width: "92vw"
-    };
-    return (
-        <div style={placeHolderStyle}></div>
-    )
-
-}
+// const NavLoadingPlaceHolder = () => {
+//     const placeHolderStyle = {
+//         height: "20vw",
+//         width: "92vw"
+//     };
+//     return (
+//         <div style={placeHolderStyle}></div>
+//     )
+//
+// }
 
 
 const Layout = ({children, isOpen, isNavFontBright, toggle}) => {
@@ -28,15 +52,17 @@ const Layout = ({children, isOpen, isNavFontBright, toggle}) => {
     return (
         <>
             {isLoading
-                ? <NavLoadingPlaceHolder>Loading</NavLoadingPlaceHolder>
-                : <NavBar isNavFontBright={isNavFontBright} isOpen={isOpen} toggle={toggle}/>
+                ? <div style={placeHolderStyle}><ClipLoader loading={true} css={override} size={100}/></div>
+                : <>
+                    <NavBar isNavFontBright={isNavFontBright} isOpen={isOpen} toggle={toggle}/>
+                    <div className={layout}>
+                        {children}
+                    </div>
+                    <footer>
+                        <p>&reg; 2022 fmcgo.pl</p>
+                    </footer>
+                </>
             }
-            <div className={layout}>
-                {children}
-                <footer>
-                    <p>&reg; 2022 fmcgo.pl</p>
-                </footer>
-            </div>
         </>
     );
 };
