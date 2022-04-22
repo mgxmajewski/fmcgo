c3d8a9517528319aa39e2d40f1617d302af7e356
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-23 00:48:55 +0200

    Fix the scroll state handler.

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,36 +24,34 @@ const IndexPage = () => {
 
     // console.log(`children.props: ` + JSON.stringify(children));
 
-    const toggleRadicalIcons = () => {
+    // const toggleRadicalIcons = () => {
+    //
+    //     const getElementHeight = name => document.getElementById(name).clientHeight;
+    //
+    //     // This is offset which makes the font to change the color when it exceeds half of its height into new section.
+    //
+    //     // NavBar edge offset adjustment
+    //     const navBarFontBottomEdge = window.innerWidth >= 900 ? window.innerWidth * .01 : -window.innerWidth * .04
+    //
+    //     // This is offset for contact form pseudo-element (gradient background)
+    //     const contactFormPseudoElementOffset = window.innerWidth * .145 - window.innerWidth * .01
+    //
+    //     // Calculations to get the right context for navBar font changes.
+    //     const goSectionHeight = getElementHeight('go-section');
+    //     const radicalSectionTopEdge = goSectionHeight + navBarFontBottomEdge
+    //     const radicalSectionHeight = getElementHeight('radical-section');
+    //     const radicalSectionBottomEdge = radicalSectionTopEdge + radicalSectionHeight
+    //     const navBarIsOverRadicalSection = window.scrollY >= radicalSectionTopEdge && window.scrollY <= radicalSectionBottomEdge
+    //
+    //     if (navBarIsOverRadicalSection) {
+    //         setAreSocialIconsVisible('false')
+    //     } else {
+    //         setAreSocialIconsVisible('true')
+    //     }
+    //
+    // }
 
-        const getElementHeight = name => document.getElementById(name).clientHeight;
-
-        // This is offset which makes the font to change the color when it exceeds half of its height into new section.
-
-        // NavBar edge offset adjustment
-        const navBarFontBottomEdge = window.innerWidth >= 900 ? window.innerWidth * .01 : -window.innerWidth * .04
-
-        // This is offset for contact form pseudo-element (gradient background)
-        const contactFormPseudoElementOffset = window.innerWidth * .145 - window.innerWidth * .01
-
-        // Calculations to get the right context for navBar font changes.
-        const goSectionHeight = getElementHeight('go-section');
-        const radicalSectionTopEdge = goSectionHeight + navBarFontBottomEdge
-        const radicalSectionHeight = getElementHeight('radical-section');
-        const radicalSectionBottomEdge = radicalSectionTopEdge + radicalSectionHeight
-        const navBarIsOverRadicalSection = window.scrollY >= radicalSectionTopEdge && window.scrollY <= radicalSectionBottomEdge
-
-        if (navBarIsOverRadicalSection) {
-            setAreSocialIconsVisible('false')
-            setIsNavFontBright('true')
-        } else {
-            setAreSocialIconsVisible('true')
-            setIsNavFontBright('false')
-        }
-
-    }
-
-    const toggleNavFontColor = () => {
+    const scrollPostionHandler = () => {
 
         const getElementHeight = name => document.getElementById(name).clientHeight;
 
@@ -89,6 +87,12 @@ const IndexPage = () => {
         const navBarIsOverBoardSection = window.scrollY >= boardSectionTopEdge && window.scrollY <= boardSectionBottemEdge
         const navBarIsOverContactForm = window.scrollY >= contactSectionFormEdge
 
+        if (navBarIsOverRadicalSection) {
+            setAreSocialIconsVisible('false')
+        } else {
+            setAreSocialIconsVisible('true')
+        }
+
         if (navBarIsOverRadicalSection || navBarIsOverBoardSection || navBarIsOverContactForm || navBarIsOverSlider) {
             setAreSocialIconsVisible('false')
             setIsNavFontBright('true')
@@ -100,8 +104,8 @@ const IndexPage = () => {
 
 
     useEffect(() => {
-        window.addEventListener('scroll', toggleNavFontColor)
-        window.addEventListener('scroll', toggleRadicalIcons)
+        window.addEventListener('scroll', scrollPostionHandler)
+        // window.addEventListener('scroll', toggleRadicalIcons)
     }, []);
 
     return (
