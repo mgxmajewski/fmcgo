1ea95ee60f2419a39348befb5be1494369e374af
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-04-23 15:29:44 +0200

    Add loader.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,19 +1,19 @@
 import React from 'react';
-// import {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import '../styles/normalize.css'
 import {layout} from "../styles/global.module.css";
 import NavBar from "./Navigation/NavBar";
 import PropTypes from "prop-types";
-// import {css} from "@emotion/react";
-// import ClipLoader from "react-spinners/ClipLoader";
+import {css} from "@emotion/react";
+import ClipLoader from "react-spinners/ClipLoader";
 //
-// const override = css`
-//   display: flex;
-//   align-self: center;
-//   justify-self: center;
-//   margin: 0 auto;
-//   border-color: #ffffff;
-// `;
+const override = css`
+  display: flex;
+  align-self: center;
+  justify-self: center;
+  margin: 0 auto;
+  border-color: #ffffff;
+`;
 
 // const loaderContainer = {
 //   display: "flex",
@@ -22,12 +22,12 @@ import PropTypes from "prop-types";
 //   height: "100vh"
 // };
 
-// const placeHolderStyle = {
-//     background: "black",
-//     display: "flex",
-//     height: "100vh",
-//     width: "100vw"
-// };
+const placeHolderStyle = {
+    background: "black",
+    display: "flex",
+    height: "100vh",
+    width: "100vw"
+};
 
 
 // const NavLoadingPlaceHolder = () => {
@@ -44,17 +44,17 @@ import PropTypes from "prop-types";
 
 const Layout = ({children, isOpen, isNavFontBright, toggle}) => {
 
-    // const [isLoading, setIsLoading] = useState(true);
-    //
-    // useEffect(() => {
-    //     setIsLoading(false)
-    // }, [])
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(false)
+    }, [])
 
     return (
-        // <>
-        //     {isLoading
-        //         ? <div style={placeHolderStyle}><ClipLoader loading={true} css={override} size={100}/></div>
-        //         :
+        <>
+            {isLoading
+                ? <div style={placeHolderStyle}><ClipLoader loading={true} css={override} size={100}/></div>
+                :
                 <>
                     <NavBar isNavFontBright={isNavFontBright} isOpen={isOpen} toggle={toggle}/>
                     <div className={layout}>
@@ -63,8 +63,8 @@ const Layout = ({children, isOpen, isNavFontBright, toggle}) => {
                     <footer>
                         <p>&reg; 2022 fmcgo.pl</p>
                     </footer>
-            {/*    </>*/}
-            {/*}*/}
+                </>
+            }
         </>
     );
 };
