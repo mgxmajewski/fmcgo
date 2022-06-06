91837d77e3fac3e87ed9a2e2600baeb17c63f7cf
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-06 14:04:45 +0200

    Adjust BoardSection.

--- src/components/BoardSection.js
+++ src/components/BoardSection.js
@@ -14,10 +14,10 @@ import "aos/dist/aos.css";
 
 const BoardSection = ({boardSectionRef}) => {
 
-    useEffect(() => {
-        AOS.init();
-        AOS.refresh();
-    }, [])
+    // useEffect(() => {
+    //     AOS.init();
+    //     AOS.refresh();
+    // }, [])
 
     return (
         <section ref={boardSectionRef} className={boardSection} id="board-section">
@@ -26,17 +26,16 @@ const BoardSection = ({boardSectionRef}) => {
                 data-aos-once="true"
                 className={boardTagline}
             >
-                Our Network<br/>
-                and Board of Directors
+                Go Network!
             </div>
             <div
                 data-aos="fade-up"
                 data-aos-once="true"
                 className={boardCopy}>
-                We work along number or specialists from different fields of marketing and advertising. Each time we
-                gather a tailor-made team of experts best suited with knowledge and experience for a given task. Our
-                Board of Directors consists of founders and owners of renowned strategy and business consulting agencies
-                and representatives from clients’ side .
+                Rekomendacje strategiczne i rozwiązania kreatywne tworzymy w oparciu o unikalną na rynku wiedzę i
+                doświadczenie naszych ekspertów - strategów, badaczy, semiotyków, mediowców, specjalistów od komunikacji
+                digital. W gronie naszych ekspertów są zarówno właściciele topowych firm badawczych i strategicznych,
+                jak i profesorowie i wykładowcy najlepszych uniwersytetów.
             </div>
             <div className={boardPictures}>
                 <div
@@ -46,7 +45,7 @@ const BoardSection = ({boardSectionRef}) => {
                     <div className={boardPersonPictureLeft}>
                         <StaticImage
                             alt={'Ana Maria Szutowicz Photo'}
-                            src="../images/director1.png"
+                            src="../images/anamariaszutowicz.png"
                             transformOptions={{
                                 fit: "cover",
                                 // cropFocus: "attention"
@@ -54,6 +53,12 @@ const BoardSection = ({boardSectionRef}) => {
                             }}
                         />
                     </div>
+                    <p data-aos="fade-up" data-aos-once="true">
+                        <b>Ana Maria Szutowicz<br/>Y&Lovers</b><br/> Etnografka i antropolożka kultury. Założycielka
+                        agencji specjalizującej się w projektowaniu i analizie badań młodych Polaków oraz doradztwie
+                        strategicznym na bazie wyników badań. Pracuje z teamem egoistycznych, roszczeniowych,
+                        uzależnionych od fidbeku i lajków badaczy.
+                    </p>
                 </div>
                 <div className={boardPersonContainer}>
                     <div
@@ -62,7 +67,7 @@ const BoardSection = ({boardSectionRef}) => {
                         className={boardPersonPictureLeft}>
                         <StaticImage
                             alt={'Henryk Klawe Photo'}
-                            src="../images/director2.png"
+                            src="../images/kaludynakaminska.png"
                             transformOptions={{
                                 fit: "cover",
                                 // cropFocus: "attention"
@@ -70,26 +75,199 @@ const BoardSection = ({boardSectionRef}) => {
                             }}
                         />
                     </div>
-                </div>
-                <div
-                    data-aos="fade-up"
-                    data-aos-once="true"
-                >
-                    <b>Ana Maria Szutowicz<br/>TGTH</b><br/> We’ve served as heads of departments, creative
-                    directors, senior creatives. <br/>For last 7 years we’ve been working together on hundreds of
-                    campaigns for brands from all categories imaginable. Along this way, we’ve discovered that what
-                    really turns us on is adverting that is meant to sell.
-                </div>
-                <div
-                    data-aos="fade-up"
-                    data-aos-once="true"
-                >
-                    <b>Henryk Klawe<br/>Y&Lovers</b><br/>We’ve served as heads of departments, creative directors,
-                    senior creatives. <br/>For last 7 years we’ve been working together on hundreds of campaigns for
-                    brands from all categories imaginable. Along this way, we’ve discovered that what really turns us on
-                    is adverting that is meant to sell.
-                </div>
+                    <p data-aos="fade-up" data-aos-once="true">
+                        <b>Klaudyna Kamińska<br/>Y&Lovers</b><br/>
 
+                    </p>
+                </div>
+                <div className={boardPersonContainer}>
+                    <div
+                        data-aos="fade-left"
+                        data-aos-once="true"
+                        className={boardPersonPictureLeft}>
+                        <StaticImage
+                            alt={'Henryk Klawe Photo'}
+                            src="../images/henrykklawe.png"
+                            transformOptions={{
+                                fit: "cover",
+                                // cropFocus: "attention"
+                                // cropFocus: 90
+                            }}
+                        />
+                    </div>
+                    <p data-aos="fade-up" data-aos-once="true">
+                        <b>Henryk Klawe<br/>TGTH</b><br/>Współzałożyciel Together odpowiedzialny za strategię. Posiada
+                        ponad 15-letnie doświadczenie w budowaniu marki i doradztwie w zakresie innowacji. Uważa, że
+                        ​​najbardziej wartościowe rozwiązania pochodzą z pracy w zgranych, multidyscyplinarnych
+                        zespołach.
+                    </p>
+                </div>
+                <div
+                    data-aos="fade-right"
+                    data-aos-once="true"
+                    className={boardPersonContainer}>
+                    <div className={boardPersonPictureLeft}>
+                        <StaticImage
+                            alt={''}
+                            src="../images/bartoszbalewski.png"
+                            transformOptions={{
+                                fit: "cover",
+                                // cropFocus: "attention"
+                                // cropFocus: 90
+                            }}
+                        />
+                    </div>
+                    <p data-aos="fade-up" data-aos-once="true">
+                        <b>Bartosz Balewski<br/>TGTH</b><br/>Jest strategiem i projektantem user experience z 12-letnim
+                        doświadczeniem, które zdobywał pracując dla klientów między innymi w USA, Hiszpanii i na Bliskim
+                        Wschodzie. Swoje projekty prowadzi zgodnie z założeniami metodologii Design Thinking.
+                    </p>
+                </div>
+                <div className={boardPersonContainer}>
+                    <div
+                        data-aos="fade-left"
+                        data-aos-once="true"
+                        className={boardPersonPictureLeft}>
+                        <StaticImage
+                            alt={'Henryk Klawe Photo'}
+                            src="../images/miloszjacyna.png"
+                            transformOptions={{
+                                fit: "cover",
+                                // cropFocus: "attention"
+                                // cropFocus: 90
+                            }}
+                        />
+                    </div>
+                    <p data-aos="fade-up" data-aos-once="true">
+                        <b>Miłosz Jacyna<br/>Social...</b><br/>
+                    </p>
+                </div>
+                <div className={boardPersonContainer}>
+                    <div
+                        data-aos="fade-left"
+                        data-aos-once="true"
+                        className={boardPersonPictureLeft}>
+                        <StaticImage
+                            alt={'Łukasz Kepiński Photo'}
+                            src="../images/lukaszkepinski.png"
+                            transformOptions={{
+                                fit: "cover",
+                                // cropFocus: "attention"
+                                // cropFocus: 90
+                            }}
+                        />
+                    </div>
+                    <p data-aos="fade-up" data-aos-once="true">
+                        <b>Łukasz Kępiński<br/>Tears of Joy / influencer&gaming</b><br/>Założyciel kreatywnej agencji
+                        influencerskiej – Tears of Joy. Dociera do TG klientów z influencer marketingiem....
+                    </p>
+                </div>
+                <div
+                    data-aos="fade-right"
+                    data-aos-once="true"
+                    className={boardPersonContainer}>
+                    <div className={boardPersonPictureLeft}>
+                        <StaticImage
+                            alt={'Professor Mirosław Filiciak Photo'}
+                            src="../images/miroslawfiliciak.png"
+                            transformOptions={{
+                                fit: "cover",
+                                // cropFocus: "attention"
+                                // cropFocus: 90
+                            }}
+                        />
+                    </div>
+                    <p data-aos="fade-up" data-aos-once="true">
+                        <b>dr hab., prof. Uniwersytetu SWPS<br/>Mirosław Filiciak</b><br/>Kierownik Katedry Kultury i
+                        Mediów.
+                        Medioznawca. Zajmuje się wpływem mediów cyfrowych na uczestnictwo w kulturze. Bada internet, gry
+                        komputerowe, przemiany telewizji oraz nieformalny obieg treści i kulturę współczesną.
+                        Współtwórca techno-kulturowego projektu Kultura 2.0.
+                    </p>
+                </div>
+                <div className={boardPersonContainer}>
+                    <div
+                        data-aos="fade-left"
+                        data-aos-once="true"
+                        className={boardPersonPictureLeft}>
+                        <StaticImage
+                            alt={'Kacper Kaźmierowicz Photo'}
+                            src="../images/kacperkazmierowicz.png"
+                            transformOptions={{
+                                fit: "cover",
+                                // cropFocus: "attention"
+                                // cropFocus: 90
+                            }}
+                        />
+                    </div>
+                    <p data-aos="fade-up" data-aos-once="true">
+                        <b>Kacper Kaźmierowicz<br/>AdPeak</b><br/>Prowadzi kampanie Google Ads. Zwolennik prostych i
+                        skutecznych rozwiązań. Testuje i optymalizuje, aby osiągnąć jak najlepszy ROAS. Jestem
+                        zwolennikiem kampanii performanceowych, czyli tych nastawionych na efekt.
+                    </p>
+                </div>
+                <div className={boardPersonContainer}>
+                    <div
+                        data-aos="fade-left"
+                        data-aos-once="true"
+                        className={boardPersonPictureLeft}>
+                        <StaticImage
+                            alt={'Wojciech Morawski Photo'}
+                            src="../images/wojciechmorawski.png"
+                            transformOptions={{
+                                fit: "cover",
+                                // cropFocus: "attention"
+                                // cropFocus: 90
+                            }}
+                        />
+                    </div>
+                    <p data-aos="fade-up" data-aos-once="true">
+                        <b>Wojciech Morawski<br/>AdPeak</b><br/>Założyciel AdPeak i Certyfikowany Trener Google.
+                        Specjalista SEM, który miał okazję optymalizować kampanie dla wielu topowych marek oraz
+                        prowadzić dla nich szkolenia (również w takich inicjatywach jak: Akademia Google Partners,
+                        SEMcamp University czy Marketing Masters).
+                    </p>
+                </div>
+                <div className={boardPersonContainer}>
+                    <div
+                        data-aos="fade-left"
+                        data-aos-once="true"
+                        className={boardPersonPictureLeft}>
+                        <StaticImage
+                            alt={'Daria Afeltowicz Photo'}
+                            src="../images/dariaafeltowicz.png"
+                            transformOptions={{
+                                fit: "cover",
+                                // cropFocus: "attention"
+                                // cropFocus: 90
+                            }}
+                        />
+                    </div>
+                    <p data-aos="fade-up" data-aos-once="true">
+                        <b>Daria Afeltowicz</b><br/>Psycholog, badacz z wykształcenia i zamiłowania. Z badaniami
+                        marketingowymi związana od 2006 roku. Przez 7 lat pracowała jako badacz w Domu Badawczym Maison
+                        wspierając firmy w budowaniu i rozwijaniu ich marek. Od 2013 roku pracowała jako specjalista
+                        badań wspierając wiedzą na temat </p>
+                </div>
+                <div className={boardPersonContainer}>
+                    <div
+                        data-aos="fade-left"
+                        data-aos-once="true"
+                        className={boardPersonPictureLeft}>
+                        <StaticImage
+                            alt={'Karolina Semiotyczka Photo'}
+                            src="../images/karolinasemiotyczka.png"
+                            transformOptions={{
+                                fit: "cover",
+                                // cropFocus: "attention"
+                                // cropFocus: 90
+                            }}
+                        />
+                    </div>
+                    <p data-aos="fade-up" data-aos-once="true">
+                        <b>Karolina<br/>Semiotyczka</b><br/> ..l.
+                    </p>
+                </div>
             </div>
 
         </section>
