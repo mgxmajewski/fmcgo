44128eff30eb0e096f77db509a49505eea6561dc
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-13 19:03:55 +0200

    Remove network refs.

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,20 +20,6 @@ const IndexPage = ({ref}) => {
     const [founderOneRef, founderOneInView] = useInView({rootMargin: '-36px'});
     const [founderTwoRef, founderTwoInView] = useInView({rootMargin: '-36px'});
     const [networkSectionRef, networkSectionInView] = useInView({rootMargin: '-36px'});
-
-    const [networkOneRef, networkOneInView] = useInView({rootMargin: '-36px'});
-    const [networkTwoRef, networkTwoInView] = useInView({rootMargin: '-36px'});
-    const [networkThreeRef, networkThreeInView] = useInView({rootMargin: '-36px'});
-    const [networkFourRef, networkFourInView] = useInView({rootMargin: '-36px'});
-    const [networkFiveRef, networkFiveInView] = useInView({rootMargin: '-36px'});
-    const [networkSixRef, networkSixInView] = useInView({rootMargin: '-36px'});
-    const [networkSevenRef, networkSevenInView] = useInView({rootMargin: '-36px'});
-    const [networkEightRef, networkEightInView] = useInView({rootMargin: '-36px'});
-    const [networkNineRef, networkNineInView] = useInView({rootMargin: '-36px'});
-    const [networkTenRef, networkTenInView] = useInView({rootMargin: '-36px'});
-    const [networkElevenRef, networkElevenInView] = useInView({rootMargin: '-36px'});
-
-
     const [contactSectionRef, contactSectionInView] = useInView();
 
     const [isOpen, setIsOpen] = useState(false);
@@ -86,44 +72,35 @@ const IndexPage = ({ref}) => {
         areSocialIconsVisible])
 
     return (
-        <Layout
-            isNavFontBright={isNavFontBright}
-            isOpen={isOpen}
-            toggle={toggleNavState}
-        >
-            <GoHero heroSectionRef={heroSectionRef}/>
-            <Radical
-                radicalRef={radicalSectionRef}
-                areSocialIconsVisible={areSocialIconsVisible}
-            />
-            <FoundersSection
-                aboveFounderOneRef={aboveFounderOneRef}
-                aboveFounderTwoRef={aboveFounderTwoRef}
-                founderOneRef={founderOneRef}
-                founderTwoRef={founderTwoRef}
-            />
-            <ExperienceSection
-                experienceSectionRef={experienceSectionRef}
-                experienceDrawersRef={experienceDrawersRef}
-                experienceInView={experienceDrawersInView}
-            />
-            <NetworkSection
-                boardSectionRef={networkSectionRef}
-                networkOneRef={networkOneRef}
-                networkTwoRef={networkTwoRef}
-                networkThreeRef={networkThreeRef}
-                networkFourRef={networkFourRef}
-                networkFiveRef={networkFiveRef}
-                networkSixRef={networkSixRef}
-                networkSevenRef={networkSevenRef}
-                networkEightRef={networkEightRef}
-                networkNineRef={networkNineRef}
-                networkTenRef={networkTenRef}
-                networkElevenRef={networkElevenRef}
-            />
-            {/*<NewsSection newsSectionRef={newsSectionRef}/>*/}
-            <GoContactSection contactSectionRef={contactSectionRef}/>
-        </Layout>
+        <>
+            <Layout
+                isNavFontBright={isNavFontBright}
+                isOpen={isOpen}
+                toggle={toggleNavState}
+            >
+                <GoHero heroSectionRef={heroSectionRef}/>
+                <Radical
+                    radicalRef={radicalSectionRef}
+                    areSocialIconsVisible={areSocialIconsVisible}
+                />
+                <FoundersSection
+                    aboveFounderOneRef={aboveFounderOneRef}
+                    aboveFounderTwoRef={aboveFounderTwoRef}
+                    founderOneRef={founderOneRef}
+                    founderTwoRef={founderTwoRef}
+                />
+                <ExperienceSection
+                    experienceSectionRef={experienceSectionRef}
+                    experienceDrawersRef={experienceDrawersRef}
+                    experienceInView={experienceDrawersInView}
+                />
+                <NetworkSection
+                    boardSectionRef={networkSectionRef}
+                />
+                {/*<NewsSection newsSectionRef={newsSectionRef}/>*/}
+                <GoContactSection contactSectionRef={contactSectionRef}/>
+            </Layout>
+        </>
     )
 }
 
