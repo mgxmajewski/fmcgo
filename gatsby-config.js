388e93c944931d16597a311978c4b10c0db89eac
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-14 17:29:49 +0200

    Add polyfills and supported browser list.

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,29 @@ module.exports = {
         "gatsby-plugin-sitemap",
         "gatsby-plugin-preload-fonts",
         {
+            resolve: "gatsby-plugin-polyfill-io",
+            options: {
+                features: [
+                    "Array.from",
+                    "Array.prototype.find",
+                    "Array.prototype.filter",
+                    "Array.prototype.findIndex",
+                    "default",
+                    "Date.prototype.toISOString",
+                    "es5",
+                    "es6",
+                    "fetch",
+                    "Object.assign",
+                    "Math.trunc",
+                    "Math.sign",
+                    "Promise",
+                    "String.prototype.repeat",
+                    "Symbol",
+                    "window.fetch"
+                ]
+            }
+        },
+        {
             resolve: `gatsby-plugin-postcss`,
             options: {
                 postCssPlugins: [require(`autoprefixer`)],
