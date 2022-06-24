ffb0c82ee2864cec18b308ca1c314f77468bf325
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2022-06-24 16:17:55 +0200

    Update network content.

--- src/components/NetworkSection.js
+++ src/components/NetworkSection.js
@@ -60,10 +60,10 @@ const NetworkSection = ({boardSectionRef}) => {
                         />
                     </div>
                     <p data-aos="fade-up" data-aos-once="true">
-                        <b>Ana Maria Szutowicz<br/>Y&Lovers</b><br/> Etnografka i antropolożka kultury. Założycielka
-                        agencji specjalizującej się w projektowaniu i analizie badań młodych Polaków oraz doradztwie
-                        strategicznym na bazie wyników badań. Pracuje z teamem egoistycznych, roszczeniowych,
-                        uzależnionych od fidbeku i lajków badaczy.
+                        <b>Ana Maria Szutowicz<br/>Y&Lovers</b><br/>
+                        CEO&Founder agencji badawczo-strategicznej Y&LOVERS
+                        Strateżka, badaczka, etnografka konsumenckiego życia naszych, ekspertka zmiany pokoleniowej i
+                        marketingu do młodych. Jurorka Effie. Członkini PTBRiO.
                     </p>
                 </div>
                 <div className={networkPersonContainer}>
@@ -78,9 +78,10 @@ const NetworkSection = ({boardSectionRef}) => {
                     </div>
                     <p data-aos="fade-up" data-aos-once="true">
                         <b>Klaudyna Kamińska<br/>Y&Lovers</b><br/>
-                        Etnografka i antropolożka kultury. Założycielka agencji specjalizującej się w projektowaniu i
-                        analizie badań młodych Polaków oraz doradztwie strategicznym na bazie wyników badań. Pracuje z
-                        teamem egoistycznych, roszczeniowych, uzależnionych od fidbeku i lajków badaczy.
+                        Copywriter, Dyrektor Kreatywny,
+                        Założyciel agencji reklamowej FMC_Go,
+                        Twórca strategii i platform komunikacji
+                        dla branży FMCG, retail i commodity services.
 
                     </p>
                 </div>
@@ -98,11 +99,10 @@ const NetworkSection = ({boardSectionRef}) => {
                         {isShowMore
                             ? <>
                                 <b>Henryk Klawe<br/>TGTH</b><br/>
-                                Współzałożyciel Together odpowiedzialny za strategię. Posiada
-                                ponad 15-letnie doświadczenie w budowaniu marki i doradztwie w zakresie innowacji.
-                                Uważa, że
-                                ​​najbardziej wartościowe rozwiązania pochodzą z pracy w zgranych, multidyscyplinarnych
-                                zespołach.
+                                Copywriter, Dyrektor Kreatywny,
+                                Założyciel agencji reklamowej FMC_Go,
+                                Twórca strategii i platform komunikacji
+                                dla branży FMCG, retail i commodity services.
                             </>
                             : <></>
                         }
@@ -123,11 +123,10 @@ const NetworkSection = ({boardSectionRef}) => {
                         {isShowMore
                             ? <>
                                 <b>Bartosz Balewski<br/>TGTH</b><br/>
-                                Jest strategiem i projektantem user experience z
-                                12-letnim
-                                doświadczeniem, które zdobywał pracując dla klientów między innymi w USA, Hiszpanii i na
-                                Bliskim
-                                Wschodzie. Swoje projekty prowadzi zgodnie z założeniami metodologii Design Thinking.
+                                Copywriter, Dyrektor Kreatywny,
+                                Założyciel agencji reklamowej FMC_Go,
+                                Twórca strategii i platform komunikacji
+                                dla branży FMCG, retail i commodity services.
                             </>
                             : <></>
                         }
@@ -135,20 +134,20 @@ const NetworkSection = ({boardSectionRef}) => {
                 </div>
                 {isShowMore
                     ? <>
-                        <div className={networkPersonContainer}>
-                            <div
-                                data-aos="fade-up"
-                                data-aos-once="true"
-                                className={networkPersonPictureLeft}>
-                                <StaticImage
-                                    alt={'Miłosz Jacyna Photo'}
-                                    src="../images/miloszjacyna.png"
-                                />
-                            </div>
-                            <p data-aos="fade-up" data-aos-once="true">
-                                <b>Miłosz Jacyna<br/>Social...</b><br/>
-                            </p>
-                        </div>
+                        {/*<div className={networkPersonContainer}>*/}
+                        {/*    <div*/}
+                        {/*        data-aos="fade-up"*/}
+                        {/*        data-aos-once="true"*/}
+                        {/*        className={networkPersonPictureLeft}>*/}
+                        {/*        <StaticImage*/}
+                        {/*            alt={'Miłosz Jacyna Photo'}*/}
+                        {/*            src="../images/miloszjacyna.png"*/}
+                        {/*        />*/}
+                        {/*    </div>*/}
+                        {/*    <p data-aos="fade-up" data-aos-once="true">*/}
+                        {/*        <b>Miłosz Jacyna<br/>Social...</b><br/>*/}
+                        {/*    </p>*/}
+                        {/*</div>*/}
                         <div className={networkPersonContainer}>
                             <div
                                 data-aos="fade-up"
@@ -160,29 +159,29 @@ const NetworkSection = ({boardSectionRef}) => {
                                 />
                             </div>
                             <p data-aos="fade-up" data-aos-once="true">
-                                <b>Łukasz Kępiński<br/>Tears of Joy / influencer&gaming</b><br/>Założyciel kreatywnej
-                                agencji
-                                influencerskiej – Tears of Joy. Dociera do TG klientów z influencer marketingiem....
+                                <b>Łukasz Kępiński<br/>Tears of Joy / influencer&gaming</b><br/>
+                                Copywriter, Dyrektor Kreatywny,
+                                Założyciel agencji reklamowej FMC_Go,
+                                Twórca strategii i platform komunikacji
+                                dla branży FMCG, retail i commodity services.
                             </p>
                         </div>
-                        <div
-                            data-aos="fade-up"
-                            data-aos-once="true"
-                            className={networkPersonContainer}>
-                            <div className={networkPersonPictureLeft}>
+
+                        <div className={networkPersonContainer}>
+                            <div
+                                data-aos="fade-up"
+                                data-aos-once="true"
+                                className={networkPersonPictureLeft}>
                                 <StaticImage
-                                    alt={'Professor Mirosław Filiciak Photo'}
-                                    src="../images/miroslawfiliciak.png"
+                                    alt={'Wojciech Morawski Photo'}
+                                    src="../images/wojciechmorawski.png"
                                 />
                             </div>
                             <p data-aos="fade-up" data-aos-once="true">
-                                <b>dr hab., prof. Uniwersytetu SWPS<br/>Mirosław Filiciak</b><br/>Kierownik Katedry
-                                Kultury i
-                                Mediów.
-                                Medioznawca. Zajmuje się wpływem mediów cyfrowych na uczestnictwo w kulturze. Bada
-                                internet, gry
-                                komputerowe, przemiany telewizji oraz nieformalny obieg treści i kulturę współczesną.
-                                Współtwórca techno-kulturowego projektu Kultura 2.0.
+                                <b>Wojciech Morawski<br/>AdPeak</b><br/>
+                                Założyciel agencji mediowej AdPeak i Certyfikowany Trener Google. Specjalista SEM,
+                                Prowadzi szkolenia w ramach Akademii Google Partners, SEMcamp University czy Marketing
+                                Masters.
                             </p>
                         </div>
                         <div className={networkPersonContainer}>
@@ -196,48 +195,46 @@ const NetworkSection = ({boardSectionRef}) => {
                                 />
                             </div>
                             <p data-aos="fade-up" data-aos-once="true">
-                                <b>Kacper Kaźmierowicz<br/>AdPeak</b><br/>Performance i Analityka to jego dwa główne
-                                obszary działalności w ekosystemie Google. Szuka rozwiązań, optymalizuje, testuje i
-                                wyciąga wnioski. Dzieli się wiedzą w Szkole Głównej Handlowej. Prywatnie możesz go
-                                znaleźć na meczach Realu Madryt.
+                                <b>Kacper Kaźmierowicz<br/>AdPeak</b><br/>
+                                Współwłaściciel agencji mediowej AdPeak. Prowadzi kampanie Google Ads. Zwolennik
+                                prostych i skutecznych rozwiązań performanceowych. Testuje i optymalizuje, aby
+                                osiągnąć jak najlepszy ROAS.
                             </p>
                         </div>
-                        <div className={networkPersonContainer}>
-                            <div
-                                data-aos="fade-up"
-                                data-aos-once="true"
-                                className={networkPersonPictureLeft}>
+                        <div
+                            data-aos="fade-up"
+                            data-aos-once="true"
+                            className={networkPersonContainer}>
+                            <div className={networkPersonPictureLeft}>
                                 <StaticImage
-                                    alt={'Wojciech Morawski Photo'}
-                                    src="../images/wojciechmorawski.png"
+                                    alt={'Professor Mirosław Filiciak Photo'}
+                                    src="../images/miroslawfiliciak.png"
                                 />
                             </div>
                             <p data-aos="fade-up" data-aos-once="true">
-                                <b>Wojciech Morawski<br/>AdPeak</b><br/>Założyciel AdPeak i Certyfikowany Trener Google.
-                                Specjalista SEM, który miał okazję optymalizować kampanie dla wielu topowych marek oraz
-                                prowadzić dla nich szkolenia (również w takich inicjatywach jak: Akademia Google
-                                Partners,
-                                SEMcamp University czy Marketing Masters).
+                                <b>dr hab., prof. Uniwersytetu SWPS<br/>Mirosław Filiciak</b><br/>
+                                Profesor Uniwersytetu SWPS, kulturoznawca, zajmuje się badaniami mediów.
                             </p>
                         </div>
-                        <div className={networkPersonContainer}>
-                            <div
-                                data-aos="fade-up"
-                                data-aos-once="true"
-                                className={networkPersonPictureLeft}>
-                                <StaticImage
-                                    alt={'Daria Afeltowicz Photo'}
-                                    src="../images/dariaafeltowicz.png"
-                                />
-                            </div>
-                            <p data-aos="fade-up" data-aos-once="true">
-                                <b>Daria Afeltowicz</b><br/>Psycholog, badacz z wykształcenia i zamiłowania. Z badaniami
-                                marketingowymi związana od 2006 roku. Przez 7 lat pracowała jako badacz w Domu Badawczym
-                                Maison
-                                wspierając firmy w budowaniu i rozwijaniu ich marek. Od 2013 roku pracowała jako
-                                specjalista
-                                badań wspierając wiedzą na temat </p>
-                        </div>
+
+                        {/*<div className={networkPersonContainer}>*/}
+                        {/*    <div*/}
+                        {/*        data-aos="fade-up"*/}
+                        {/*        data-aos-once="true"*/}
+                        {/*        className={networkPersonPictureLeft}>*/}
+                        {/*        <StaticImage*/}
+                        {/*            alt={'Daria Afeltowicz Photo'}*/}
+                        {/*            src="../images/dariaafeltowicz.png"*/}
+                        {/*        />*/}
+                        {/*    </div>*/}
+                        {/*    <p data-aos="fade-up" data-aos-once="true">*/}
+                        {/*        <b>Daria Afeltowicz</b><br/>Psycholog, badacz z wykształcenia i zamiłowania. Z badaniami*/}
+                        {/*        marketingowymi związana od 2006 roku. Przez 7 lat pracowała jako badacz w Domu Badawczym*/}
+                        {/*        Maison*/}
+                        {/*        wspierając firmy w budowaniu i rozwijaniu ich marek. Od 2013 roku pracowała jako*/}
+                        {/*        specjalista*/}
+                        {/*        badań wspierając wiedzą na temat </p>*/}
+                        {/*</div>*/}
                         <div className={networkPersonContainer}>
                             <div
                                 data-aos="fade-up"
@@ -249,7 +246,10 @@ const NetworkSection = ({boardSectionRef}) => {
                                 />
                             </div>
                             <p data-aos="fade-up" data-aos-once="true">
-                                <b>Karolina<br/>Semiotyczka</b><br/> ..l.
+                                <b>Karolina<br/>Semiotyczka</b><br/>
+                                Semiotyczka, antropolożka, badaczka. Analizuje internetowy, medialny i popkulturowy
+                                dyskurs konsumencki oraz komunikację marek. Badając znaki językowe i wizualne, szuka
+                                kodów kulturowych. Łowi trendy i kierunki zmian.
                             </p>
                         </div>
                     </>
