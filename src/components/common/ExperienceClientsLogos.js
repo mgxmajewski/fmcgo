f5f343fad74e81094d6ff919eb8ca6e3aef7a8fc
agilecat-io <agilecatio@gmail.com>
2024-07-04 20:09:07 +0200

    Change layout of clients logos to grid

--- src/components/common/ExperienceClientsLogos.js
+++ src/components/common/ExperienceClientsLogos.js
@@ -1,43 +1,40 @@
 import React from 'react';
 import {StaticImage} from "gatsby-plugin-image";
-import {
-    experienceClients,
-    experienceLogos,
-    experienceLogosCenteredRow
-} from '../../styles/experience-section.module.css'
+import {experienceClients, spanCols2, spanCols2Skip1} from '../../styles/experience-section.module.css'
 
 const ExperienceClientsLogos = () => {
     return (
         <div className={experienceClients}>
-            <div className={experienceLogos}>
                 <StaticImage
                     alt={'Develey logo'}
                     src="../../images/develey.png"
-                    objectFit="contain"
+                    objectFit="cover"
+                    className={spanCols2}
                 />
                 <StaticImage
                     alt={'Tchibo logo'}
                     src="../../images/tchibo.png"
-                    objectFit="contain"
+                    objectFit="cover"
+                    className={spanCols2}
                 />
                 <StaticImage
                     alt={'Travelist.pl logo'}
                     src="../../images/travelist.png"
-                    objectFit="contain"
+                    objectFit="cover"
+                    className={spanCols2}
                 />
-            </div>
-            <div className={experienceLogosCenteredRow}>
                 <StaticImage
                     alt={'Archicom logo'}
                     src="../../images/archicom.jpeg"
-                    objectFit="contain"
+                    objectFit="cover"
+                    className={spanCols2Skip1}
                 />
                 <StaticImage
                     alt={'Triverna logo'}
                     src="../../images/triverna.jpeg"
-                    objectFit="contain"
+                    objectFit="cover"
+                    className={spanCols2}
                 />
-            </div>
         </div>
     );
 };
